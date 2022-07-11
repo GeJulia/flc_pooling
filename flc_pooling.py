f7fafbec20442fd4361dad550b2dbf38733d4a82
GeJulia <jgr08@gmx.de>
2022-07-11 11:09:57 +0200

    flc pooling and flc preact resnet

---
+++ flc_pooling.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FLC_Pooling(nn.Module):
+    # pooling torugh selecting only the low frequent part in the fourier domain and only using this part to go back into the spatial domain
+    # save computations as we do not need to do the downsampling trough conv with stride 2
+    def __init__(self):
+        super(FLC_Pooling, self).__init__()
+
+    def forward(self, x):
+
+        low_part = torch.fft.fftshift(torch.fft.fft2(x))[:,:,int(x.size()[2]/4):int(x.size()[2]/4*3),int(x.size()[3]/4):int(x.size()[3]/4*3)]
+        
+        return abs(torch.fft.ifft2(torch.fft.ifftshift(low_part)))
---
+++ preact_resnet_flc.py
@@ -0,0 +1,123 @@
+'''Generic Class for PreAct ResNet with FLC Pooling'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from flc_pooling import FLC_Pooling
+
+
+class PreActBlock(nn.Module):
+    '''Pre-activation version of the BasicBlock.'''
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, drop=0):
+        super(PreActBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        if stride == 1:
+            self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        else:
+            self.conv1 = nn.Sequential( 
+                FLC_Pooling(),
+                nn.Conv2d(in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False))
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                FLC_Pooling(),
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=1, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(x) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out += shortcut
+        return out
+
+
+class PreActBottleneck(nn.Module):
+    '''Pre-activation version of the original Bottleneck module.'''
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, drop=0):
+        super(PreActBottleneck, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        if stride == 1:
+            self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        else:
+            nn.Sequential(
+                FLC_Pooling(), 
+                nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False))
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
+
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                FLC_Pooling(),
+                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=1, bias=False)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(x))
+        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
+        out = self.conv1(out)
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = self.conv3(F.relu(self.bn3(out)))
+        out += shortcut
+        return out
+
+
+class PreActResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, drop=0):
+        super(PreActResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.bn = nn.BatchNorm2d(512 * block.expansion)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.relu(self.bn(out))
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
+def PreActResNet18(num_classes=10):
+    return PreActResNet(PreActBlock, [2,2,2,2], num_classes=num_classes)
+
+
+
+class PreActResNet_normalized(PreActResNet):
+    def __init__(self, block, num_blocks, num_classes=10, mu=[0.4914, 0.4822, 0.4465], sigma=[0.2471, 0.2435, 0.2616], device='cuda'):
+        super(PreActResNet_normalized, self).__init__(block=block, num_blocks=num_blocks, num_classes=num_classes)
+        self.mu = torch.Tensor(mu).float().view(3, 1, 1).to(device)
+        self.sigma = torch.Tensor(sigma).float().view(3, 1, 1).to(device)
+    
+    def forward(self, x):
+        x = (x - self.mu) / self.sigma
+        return super(PreActResNet_normalized, self).forward(x)
+
+def PreActResNet18_normalized(num_classes=10, mu=[0.4914, 0.4822, 0.4465], sigma=[0.2471, 0.2435, 0.2616], device='cuda'):
+    return PreActResNet_normalized(PreActBlock, [2,2,2,2], num_classes=num_classes, mu=mu, sigma=sigma, device=device)
